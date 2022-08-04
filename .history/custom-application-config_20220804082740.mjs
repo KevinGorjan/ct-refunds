98276354fdc4f4711beffb5efb9dc887de29a502
Kevin Gorjan <kevingorjan@gmail.com>
2022-08-04 08:31:13 +0200

    init project

---
+++ .history/custom-application-config_20220804082740.mjs
@@ -0,0 +1,43 @@
+import { PERMISSIONS, entryPointUriPath } from './src/constants';
+
+/**
+ * @type {import('@commercetools-frontend/application-config').ConfigOptions}
+ */
+const config = {
+  name: 'Refunds',
+  entryPointUriPath,
+  cloudIdentifier: 'gcp-eu',
+  env: {
+    development: {
+      initialProjectKey: 'prj-new-horizon-dev',
+    },
+
+    production: {
+      applicationId: 'TODO',
+      url: 'https://your_app_hostname.com',
+    },
+  },
+
+  oAuthScopes: {
+    view: ['view_products'],
+    manage: ['manage_products'],
+  },
+
+  icon: '${path:@commercetools-frontend/assets/application-icons/user.svg}',
+  mainMenuLink: {
+    defaultLabel: 'Refunds',
+    labelAllLocales: [],
+    permissions: [PERMISSIONS.View],
+  },
+
+  submenuLinks: [
+    {
+      uriPath: 'channels',
+      defaultLabel: 'Channels',
+      labelAllLocales: [],
+      permissions: [PERMISSIONS.View],
+    },
+  ],
+};
+
+export default config;
